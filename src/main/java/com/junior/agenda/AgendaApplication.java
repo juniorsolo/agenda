e00887cff36590bd8cf55e2d5b7251806f7e5626
Junior <junior.solo@hotmail.com>
2024-07-20 18:19:18 -0300

    Adicionando dependencias e conectando com o mongoDB

---
+++ src/main/java/com/junior/agenda/AgendaApplication.java
@@ -0,0 +1,14 @@
+package com.junior.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaApplication.class, args);
+		System.out.println("Hello Word!!!");
+	}
+
+}
